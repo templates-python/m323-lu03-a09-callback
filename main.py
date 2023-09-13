2056be13c1207a2e46f9e855c6d36050c98e0f5d
Kevin Maurizi <kevin.maurizi@bzz.ch>
2023-09-13 13:55:10 +0200

    done

--- main.py
+++ main.py
@@ -1,12 +1,50 @@
-def calculate():
-    """
-    calculates the total
-    :return: None
-    """
-    price = 15.75
-    quantity = 3  # TODO change quantity to 5
-    print(f'Total: {price * quantity}')
+import tkinter as tk
 
+class GreetingApp:
+    """
+    Eine einfache GUI-Anwendung, die es einem Benutzer ermöglicht, seinen Namen in ein Eingabefeld einzugeben.
+    Nach Eingabe seines Namens und Klick auf einen Button wird eine Begrüßungsnachricht angezeigt.
+    """
+
+    def __init__(self, root):
+        """
+        Initialisiert die GUI-Komponenten.
+
+        Args:
+        - root (tk.Tk): Das Hauptfenster der Anwendung.
+        """
+
+
+        # Label-Widget, das den Benutzer auffordert, seinen Namen einzugeben
+        #TODO
+
+        # Eingabefeld-Widget (Entry-Widget) für den Namen des Benutzers
+        # TODO
+
+        # Label-Widget für die Begrüßungsnachricht
+        # Zu Beginn wird kein Text angezeigt
+        # TODO
+
+        # Button-Widget, das die greet_user-Methode aufruft, wenn es geklickt wird
+        # TODO
+
+    def greet_user(self):
+        """
+        Diese Methode wird aufgerufen, wenn der Benutzer auf den "Begrüßen"-Button klickt.
+        Sie liest den Namen aus dem Eingabefeld aus und zeigt eine Begrüßungsnachricht unterhalb des Buttons an.
+        """
+        # Den eingegebenen Namen aus dem Entry-Widget holen
+
+        # Begrüßungsnachricht generieren
+
+        # Die Begrüßungsnachricht im Label-Widget anzeigen
 
 if __name__ == '__main__':
-    calculate()
+    # Hauptfenster des GUI erstellen
+    root = tk.Tk()
+
+    # Instanz der GreetingApp-Klasse erstellen
+    app = GreetingApp(root)
+
+    # GUI-Event-Loop starten
+    root.mainloop()
--- main_test.py
+++ main_test.py
@@ -1,6 +1,27 @@
-import main
+import unittest
+from main import GreetingApp
+import tkinter as tk
 
-def test(capsys):
-    main.calculate()
-    captured = capsys.readouterr()
-    assert captured.out == "Total: 78.75\n"
+
+class TestGreetingApp(unittest.TestCase):
+
+    def setUp(self):
+        self.root = tk.Tk()
+        self.app = GreetingApp(self.root)
+
+    def tearDown(self):
+        self.root.destroy()
+
+    def test_greet_user(self):
+        # Setze den Namen im Eingabefeld
+        self.app.entry.insert(0, "John")
+
+        # Rufe die Callback-Funktion auf
+        self.app.greet_user()
+
+        # Überprüfe, ob die Begrüßungsnachricht korrekt angezeigt wird
+        self.assertEqual(self.app.greeting_label.cget("text"), "Hallo, John!")
+
+
+if __name__ == '__main__':
+    unittest.main()
