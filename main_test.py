d7d34442c3c207d654d2eca813af4c5548d47541
Kevin Maurizi <kevin.maurizi@bzz.ch>
2023-09-22 11:49:23 +0200

    changed main_test.py

--- main_test.py
+++ main_test.py
@@ -1,32 +1,33 @@
-# test_greeting_app.py
-
 import pytest
-
+from main import GreetingApp
 
 # Mock für das Hauptfenster von tkinter
 @pytest.fixture
 def mock_tk(mocker):
-    mock = mocker.patch('tkinter.Tk', autospec=True, create=True)()
-    mock.tk = mocker.MagicMock()  # Hinzufügen des 'tk'-Attributs
-    mock.children = {}  # Hinzufügen des 'children'-Attributs
+    try:
+        mock = mocker.patch('tkinter.Tk', autospec=True, create=True)()
+        mock.tk = mocker.MagicMock()  # Hinzufügen des 'tk'-Attributs
+        mock.children = {}  # Hinzufügen des 'children'-Attributs
+    except ModuleNotFoundError:
+        mock = mocker.MagicMock()
+        mock.tk = mocker.MagicMock()  # Hinzufügen des 'tk'-Attributs
+        mock.children = {}  # Hinzufügen des 'children'-Attributs
     return mock
-
 
 # Mock für den Button von tkinter
 @pytest.fixture
 def mock_button(mocker):
     return mocker.patch('tkinter.Button', autospec=True, create=True)()
 
+@pytest.fixture
+def mock_label(mocker):
+    return mocker.patch('tkinter.Label', autospec=True, create=True)()
 
-def test_greet_user(mock_tk, mock_button, mocker):
-    # Mock the entire tkinter module
-    mocker.patch('main.tk', mocker.MagicMock())
+@pytest.fixture
+def mock_entry(mocker):
+    return mocker.patch('tkinter.Entry', autospec=True, create=True)()
 
-    # Now import the GreetingApp class
-    from main import GreetingApp
-
-    mocker.patch('tkinter.Label', autospec=True, create=True)
-    mocker.patch('tkinter.Entry', autospec=True, create=True)
+def test_greet_user(mock_tk, mock_button, mock_label, mock_entry, mocker):
     app = GreetingApp(mock_tk)
 
     # Finden des richtigen Entry-Widgets und Mocken der get-Methode
